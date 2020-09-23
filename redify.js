d0e6f55bcd5a934c895cf57edf6c9b3b523c450d
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-23 21:58:50 +0100

    done

---
+++ redify.js
@@ -0,0 +1,36 @@
+const themes = {
+    'live': {
+      colors: {
+        frame: "#CD3333"
+      }
+    }
+  };
+
+browser.tabs.onActivated.addListener(event => update(event.tabId));
+browser.tabs.onUpdated.addListener(tabId => update(tabId));  
+
+
+// async function getCurrentThemeInfo() {
+//   
+//   console.log('im here');
+// }
+
+var current = null;
+
+async function update(tabId) {    
+    if (current == null) {
+        current = await browser.theme.getCurrent();    
+    }
+    
+    let tab = await browser.tabs.get(tabId);        
+    let windowId = tab.windowId;
+    var url = tab.url;
+
+    if (tab.active &&  /.simplybusiness.live/.test(url)) {
+        browser.theme.update(windowId, themes.live);        
+    } else {
+        browser.theme.update(windowId, current);        
+        // browser.theme.reset(windowId);  
+    }    
+}
+  
